35f2df9924908373aa92bbbeb9096af6a8812e34
afshanahmeda2k <afshanahmeda2k@gmail.com>
2021-05-28 09:40:11 +0530

    All slides done

--- dist/impress_ppt.e31bb0bc.js
+++ dist/impress_ppt.e31bb0bc.js
@@ -147,7 +147,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40331" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "39021" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
